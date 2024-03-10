5eb3b0b2f6ce5868f7e952f261c42ec47b44a288
vrostar <mistermustache33@gmail.com>
2024-03-10 17:55:44 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+
+// Assuming the comma-separated values represent data points
+const data = [
+    { name: '1', value1: 4.16, value2: -23.5, value3: -32.79 },
+    { name: '2', value1: 4.78, value2: -24.18, value3: -34.54 },
+    // ... add remaining data points here following the same structure
+    { name: '33', value1: 33.14, value2: -13.82, value3: -77.98 },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // No state needed for this static data
+
+    return (
+        <div className="App">
+            <LineChart width={600} height={300} data={data}>
+                {/* Assuming you want to plot value1 (first value) vs name */}
+                <Line type="monotone" dataKey="value1" stroke="#555" name="Value 1" />
+                {/* Add more lines here to plot other data series (value2, value3) if needed */}
+                <XAxis dataKey="name" />
+                <YAxis />
+            </LineChart>
+        </div>
+    );
 }
 
 export default App;
